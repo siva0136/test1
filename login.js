0be2a1e1bf871ad94f58aa8c225d0e03ef26d35b
gevini purushotham <dudduguntasiva62@gmail.com>
2024-08-08 21:43:41 +0530

    login desing

---
+++ login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      <div>
+        user Id : <input/>
+      </div>
+      <div>
+        password Id : <input/>
+      </div>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default login
